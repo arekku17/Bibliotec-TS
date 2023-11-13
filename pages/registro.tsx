6b00715dc576579b60940d8cc494668386ac231c
AxelHuesca <axelblooder@gmail.com>
2023-11-13 17:45:54 -0600

    avances

--- pages/registro.tsx
+++ pages/registro.tsx
@@ -1,12 +1,12 @@
 import { Inter } from 'next/font/google'
 
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Registro() {
   return (
-    <>
       <h1>Registro</h1>
-    </>
+      
   )
 }
