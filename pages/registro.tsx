0e5974fa0ff8d1ca437c5494a7f36da339896fd9
AxelHuesca <axelblooder@gmail.com>
2023-11-23 18:54:24 -0600

    avances#3

--- pages/registro.tsx
+++ pages/registro.tsx
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 
 
@@ -6,7 +8,23 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Registro() {
   return (
-      <h1>Registro</h1>
+    <div>
+   <h1>Historial de registros de la biblioteca</h1>
+      
+
+      <DataTable value={
+        []
+      } paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem', backgroundColor: "black" }}>
+        <Column field="name" header="Fecha" style={{ width: '25%' }}></Column>
+        <Column field="country.name" header="Hora de entrada" style={{ width: '25%' }}></Column>
+        <Column field="company" header="Hora de salida" style={{ width: '25%' }}></Column>
+        <Column field="representative.name" header="Nombre" style={{ width: '25%' }}></Column>
+      </DataTable>
+      
+    </div>
+   
+         
+
       
   )
 }
--- pages/servicios.tsx
+++ pages/servicios.tsx
@@ -1,12 +1,38 @@
 import { Inter } from 'next/font/google'
-
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+        
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Servicios() {
   return (
-    <>
-      <h1>Servicios</h1>
-    </>
+    
+    <div className="grid h-full">
+      <div className="col-12 md:col-6">
+        <div className='card'>
+          <h1 className='text-800 text-2xl text-left'>Servicios de la biblioteca</h1>
+
+          <div className="col-12 md:col-6">
+          <i className="pi pi-desktop" style={{ fontSize: '10rem' }}></i>
+        
+          <div className="col-12 md:col-6">
+          <Button label="Solicitar PC de consulta" icon="pi pi-external-link" onClick={() => setVisible(true)} />
+          <Dialog header="Header" visible={()} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+          <p className="m-0">
+        
+          </p>
+          </Dialog>
+          </div>
+
+            <div className="col-12 md:col-6">
+            <i className="pi pi-table" style={{ fontSize: '10rem' }}></i>
+            <Button label="Solicitar juego de mesa" />
+            </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
